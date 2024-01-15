c6dfe0f63deb89045e73a3fc3fd4ed97bc1f2c3c
sandeepcodehive <sandeep.prajapati@codehivetech.com>
2024-01-15 23:07:55 +0530

    refactor navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import "./Navbar.css"
 
 
 export default function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary custom-navbar">
             <div className="container-fluid">
                 <a className="navbar-brand" href="/">Navbar</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,34 +15,22 @@ export default function Navbar() {
                         <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="/">Home</a>
                         </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Startup
-                            </a>
-                            <ul className='dropdown-menu'>
-                                <li><a className="dropdown-item" href="/">Startup Pro.</a></li>
-                                <li><a className="dropdown-item" href="/">Startup Details</a></li>
-                                <li><a className="dropdown-item" href="/">Connect Us</a></li>
-                            </ul>
+                        <li className="nav-item">
+                            <a className="nav-link " aria-current="page" href="/">StartUps</a>
                         </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Investor
-                            </a>
-                            <ul className="dropdown-menu dropdown-menu-end">
-                                <li><a className="dropdown-item" href="/">Find Inv.</a></li>
-                                <li><a className="dropdown-item" href="/">Retail Inv.</a></li>
-                                <li><a className="dropdown-item" href="/">FAQs</a></li>
-                            </ul>
+                        <li className="nav-item">
+                            <a className="nav-link " aria-current="page" href="/">Investor</a>
                         </li>
                         <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Resources
                             </a>
                             <ul className="dropdown-menu dropdown-menu-end">
-                                <li><a className="dropdown-item" href="/">Community</a></li>
-                                <li><a className="dropdown-item" href="/">Learn/Webinar</a></li>
+                                {/* <li><a className="dropdown-item" href="/">Community</a></li> */}
+                                {/* <li><a className="dropdown-item" href="/">Learn/Webinar</a></li> */}
                                 <li><a className="dropdown-item" href="/">News</a></li>
+                                <li><a className="dropdown-item" href="/">Connect Us</a></li>
+                                <li><a className="dropdown-item" href="/">FAQs</a></li>
                             </ul>
                         </li>
                         <li className="nav-item">
