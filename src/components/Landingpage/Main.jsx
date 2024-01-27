a4aeeb51500da8ed5e032e214ec7256d50fbb32d
sandeepcodehive <sandeep.prajapati@codehivetech.com>
2024-01-27 20:13:34 +0530

    feat main image

--- src/components/Landingpage/Main.jsx
+++ src/components/Landingpage/Main.jsx
@@ -3,92 +3,11 @@ import "./Main.css";
 
 export default function Main() {
   return (
-    <div>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6">
-            <div className="content">
-              <h1 className="title">Find Your Next Investment</h1>
-              <p className="desc">
-                Identify high-growth startups and industry trends so you know
-                where to invest.
-              </p>
-              <button>Click</button>
-            </div>
-          </div>
-          <div className="col-md-6">
-            <div className="image">
-              <img
-                src="/img/fin1.png"
-                alt="find investor"
-                className="find-img img-fluid"
-              />
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-6">
-            <div className="image">
-              <img
-                src="/img/fin1.png"
-                alt="find investor"
-                className="find-img img-fluid"
-              />
-            </div>
-          </div>
-          <div className="col-md-6">
-            <div className="content">
-              <h1 className="title">Find Your Next Investment</h1>
-              <p className="desc">
-                Identify high-growth startups and industry trends so you know
-                where to invest.
-              </p>
-              <button>Click</button>
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-6">
-            <div className="content">
-              <h1 className="title">Find Your Next Investment</h1>
-              <p className="desc">
-                Identify high-growth startups and industry trends so you know
-                where to invest.
-              </p>
-              <button>Click</button>
-            </div>
-          </div>
-          <div className="col-md-6">
-            <div className="image">
-              <img
-                src="/img/fin1.png"
-                alt="find investor"
-                className="find-img img-fluid"
-              />
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-6">
-            <div className="image">
-              <img
-                src="/img/fin1.png"
-                alt="find investor"
-                className="find-img img-fluid"
-              />
-            </div>
-          </div>
-          <div className="col-md-6">
-            <div className="content">
-              <h1 className="title">Find Your Next Investment</h1>
-              <p className="desc">
-                Identify high-growth startups and industry trends so you know
-                where to invest.
-              </p>
-              <button>Click</button>
-            </div>
-          </div>
-        </div>
+    <div className="container">
+      <p className="title">Way of Businesses! <br/> Contect US</p>
+      <div className="but">
+        <button className="start">Start Up</button>
+        <button className="invest">Investor</button>
       </div>
     </div>
   );
