4e6d800283201755ce983958287d523bfa92cd76
Uday1004 <Udaysolanki530@gmail.com>
2024-01-28 11:31:46 +0530

    navbar and section updated

--- src/components/Landingpage/Main.jsx
+++ src/components/Landingpage/Main.jsx
@@ -1,14 +1,39 @@
 import React from "react";
 import "./Main.css";
+import img from "../Landingpage/image/main-vector-removebg-preview.png";
 
 export default function Main() {
   return (
-    <div className="container">
-      <p className="title">Way of Businesses! <br/> Contect US</p>
-      <div className="but">
-        <button className="start">Start Up</button>
-        <button className="invest">Investor</button>
+    <header class="bg-dark py-5">
+      <div class="container px-5">
+        <div class="row gx-5 align-items-center justify-content-center">
+          <div class="col-lg-8 col-xl-7 col-xxl-6">
+            <div class="my-5 text-center text-xl-start">
+              <h1 class="display-5 fw-bolder text-white mb-2">
+                Ayush Bharat
+              </h1>
+              <h1 class=" display-5 lead fw-normal text-white-50 mb-4">
+                Startup India Platform
+              </h1>
+              <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
+                <a class="btn btn-primary btn-lg px-4 me-sm-3" href="#features">
+                   For Investor
+                </a>
+                <a class="btn btn-outline-light btn-lg px-4" href="#!">
+                  For Startup
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-5 col-xxl-6 d-none d-xl-block text-center">
+            <img
+              class="img-fluid rounded-3 my-5"
+              src={img}
+              alt="..."
+            />
+          </div>
+        </div>
       </div>
-    </div>
+    </header>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,42 +4,30 @@ import "./Navbar.css"
 
 export default function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary custom-navbar">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="/">Navbar</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse navtop" id="navbarNav">
-                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link " aria-current="page" href="/">StartUps</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link " aria-current="page" href="/">Investor</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Resources
-                            </a>
-                            <ul className="dropdown-menu dropdown-menu-end">
-                                {/* <li><a className="dropdown-item" href="/">Community</a></li> */}
-                                {/* <li><a className="dropdown-item" href="/">Learn/Webinar</a></li> */}
-                                <li><a className="dropdown-item" href="/">News</a></li>
-                                <li><a className="dropdown-item" href="/">Connect Us</a></li>
-                                <li><a className="dropdown-item" href="/">FAQs</a></li>
-                            </ul>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" aria-current="page" href="/">Login</a>
-                        </li>
-                    </ul>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div class="container px-5">
+                    <a class="navbar-brand" href="index.html">Ayush-Bharat</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
+                            <li class="nav-item"><a class="nav-link" href="about.html">Startup</a></li>
+                            <li class="nav-item"><a class="nav-link" href="contact.html">Investor</a></li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" id="navbarDropdownBlog" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Resources</a>
+                                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownBlog">
+                                    <li><a class="dropdown-item" href="blog-home.html">News</a></li>
+                                    <li><a class="dropdown-item" href="blog-post.html">Connect Us</a></li>
+                                    <li><a class="dropdown-item" href="blog-post.html">FAQs</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item"><a class="nav-link" href="pricing.html">Help?</a></li>
+                            <li class="nav-item"><a class="nav-link" href="faq.html">Login</a></li>
+                             
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
 
     )
 }
